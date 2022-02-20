d8c1420faf76e9a1c607980ec7417af25f4e009d
sunnydsouza <dsouzasunny_1436@yahoo.co.in>
2022-02-20 10:38:31 +0530

    feat: Added sample feature 2

---
+++ src/main/java/com/sunnydsouza/github_actions/SampleClass2.java
@@ -0,0 +1,7 @@
+package com.sunnydsouza.github_actions;
+
+public class SampleClass2 {
+    public static void main(String[] args) {
+        System.out.println("This is a new sample feature 2");
+    }
+}
