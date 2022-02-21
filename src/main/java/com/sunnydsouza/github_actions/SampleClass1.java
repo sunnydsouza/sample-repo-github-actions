cf2da002ac2aeed04611e8dd0c46338d8720dbc0
sunnydsouza <dsouzasunny_1436@yahoo.co.in>
2022-02-21 14:12:32 +0530

    fix: added fix for a feature feat: added feature 3

--- src/main/java/com/sunnydsouza/github_actions/SampleClass1.java
+++ src/main/java/com/sunnydsouza/github_actions/SampleClass1.java
@@ -4,4 +4,8 @@ public class SampleClass1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    void fix(int a, int b) {
+        System.out.println("Dummy fix:"+a + b);
+    }
 }
