016246e4653ea5496a3c7c830433876e12d875bc
sunnydsouza <dsouzasunny_1436@yahoo.co.in>
2022-02-22 08:02:04 +0530

    fix(release): added below fixes
    Fix1 dummy fix 1
    Fix2 dummy fix 2

--- src/main/java/com/sunnydsouza/github_actions/SampleClass1.java
+++ src/main/java/com/sunnydsouza/github_actions/SampleClass1.java
@@ -9,4 +9,14 @@ public class SampleClass1 {
         System.out.println("Dummy fix:"+a + b);
         System.out.println("Testing a fake fix wherein the commit message does not conform to semantic-release");
     }
+
+    void fix2(int a, int b) {
+        System.out.println("Dummy fix:"+a + b);
+        System.out.println("Testing a fake fix wherein the commit message does not conform to semantic-release");
+    }
+
+    void fix3(int a, int b) {
+        System.out.println("Dummy fix:"+a + b);
+        System.out.println("Testing a fake fix wherein the commit message does not conform to semantic-release");
+    }
 }
