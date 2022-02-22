2c58e6485b05129305803524ec78acdfade46dcf
sunnydsouza <dsouzasunny_1436@yahoo.co.in>
2022-02-22 08:03:12 +0530

    perf(release): added performance improvements
    BREAKING CHANGE: existing endpoints will change
    Please take backup before proceeding

--- src/main/java/com/sunnydsouza/github_actions/SampleClass2.java
+++ src/main/java/com/sunnydsouza/github_actions/SampleClass2.java
@@ -4,4 +4,9 @@ public class SampleClass2 {
     public static void main(String[] args) {
         System.out.println("This is a new sample feature 2");
     }
+
+    void perfImprovements(int a, int b) {
+        System.out.println("Dummy fix:"+a + b);
+        System.out.println("Testing a fake fix wherein the commit message does not conform to semantic-release");
+    }
 }
