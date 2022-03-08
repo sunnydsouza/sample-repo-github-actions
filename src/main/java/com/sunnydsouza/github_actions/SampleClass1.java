da8a870f6f2958af756ba24e04c98cacbccc2a39
sunnydsouza <dsouzasunny_1436@yahoo.co.in>
2022-03-08 07:53:40 +0530

    fix(feature): fix 5 released

--- src/main/java/com/sunnydsouza/github_actions/SampleClass1.java
+++ src/main/java/com/sunnydsouza/github_actions/SampleClass1.java
@@ -25,4 +25,8 @@ public class SampleClass1 {
         System.out.println("Testing a fake fix wherein the commit message does not conform to semantic-release");
     }
 
+    void fix5(int a, int b) {
+        System.out.println("Dummy fix:"+a + b);
+        System.out.println("Testing a fake fix wherein the commit message does not conform to semantic-release");
+    }
 }
