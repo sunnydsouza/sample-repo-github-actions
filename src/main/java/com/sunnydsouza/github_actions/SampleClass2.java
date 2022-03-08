7fefed9e671bfc7a12c727e9f92972802691e667
sunnydsouza <dsouzasunny_1436@yahoo.co.in>
2022-03-08 07:18:38 +0530

    feat(feature2): added feature3 to SampleClass2

--- src/main/java/com/sunnydsouza/github_actions/SampleClass2.java
+++ src/main/java/com/sunnydsouza/github_actions/SampleClass2.java
@@ -12,6 +12,11 @@ public class SampleClass2 {
     void addedNewFeature2(){
         System.out.println("This is a new sample feature 2");
     }
+
+    void addedNewFeature3(){
+        System.out.println("This is a new sample feature 3");
+    }
+
     void perfImprovements(int a, int b) {
         System.out.println("Dummy fix:"+a + b);
         System.out.println("Testing a fake fix wherein the commit message does not conform to semantic-release");
